5baffef9ea45272f37805b77876e14f9acf4700e
Jeffrey Houdijk <jhoudijk@schubergphilis.com>
2022-05-06 09:10:46 +0200

    disable monitoring for now:

--- aurora.tf
+++ aurora.tf
@@ -104,13 +104,13 @@ module "rds_user_management" {
   }
 }
 
-module "rds_monitoring" {
-  count             = "${var.enable_cloudwatch_monitoring == true ? 1 : 0}"
-  source            = "app.terraform.io/ccv-group/rds-monitoring/aws"
-  version           = "1.0.0"
-  email_endpoint    = var.email_endpoint
-  kms_key_id        = var.kms_key_arn
-  rds_instance_ids  = module.rds_aurora.*.instance_ids
-  send_email_alerts = "${length(var.email_endpoint) > 0 ? true : false}"
-  tags              = var.tags
-}
+#module "rds_monitoring" {
+#  count             = "${var.enable_cloudwatch_monitoring == true ? 1 : 0}"
+#  source            = "app.terraform.io/ccv-group/rds-monitoring/aws"
+#  version           = "1.0.0"
+#  email_endpoint    = var.email_endpoint
+#  kms_key_id        = var.kms_key_arn
+#  rds_instance_ids  = module.rds_aurora.*.instance_ids
+#  send_email_alerts = "${length(var.email_endpoint) > 0 ? true : false}"
+#  tags              = var.tags
+#}
